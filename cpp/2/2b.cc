6d00c51f3c8ddb6ed6fa083b07c144fd77b5cef6
Chris Kennelly <ckennelly@ckennelly.com>
2021-12-02 00:13:47 -0500

    2021: Day 2

---
+++ cpp/2/2b.cc
@@ -0,0 +1,28 @@
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+
+#include "absl/strings/string_view.h"
+
+int main(int argc, char **argv) {
+    int x = 0, y = 0, aim = 0;
+
+    char action[256];
+	int value;
+	while (fscanf(stdin, "%s %d", action, &value) == 2) {
+        absl::string_view a(action);
+        if (a == "forward") {
+            x += value;
+            y += aim * value;
+        } else if (a == "down") {
+            aim += value;
+        } else if (a == "up") {
+            aim -= value;
+        } else {
+            assert(false);
+        }
+	}
+
+	printf("%d\n", std::abs(x * y));
+}
