457985fe8a1351ec6c28eda6dad0e6a7b459cca2
Chris Kennelly <ckennelly@ckennelly.com>
2021-12-01 20:49:48 -0500

    2021: Day 1

---
+++ cpp/1/1a.cc
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <vector>
+
+int main(int argc, char **argv) {
+	std::vector<int> values;
+	int value;
+	while (fscanf(stdin, "%d", &value) == 1) {
+		values.push_back(value);
+	}
+
+    int increases = 0;
+    for (int i = 1; i < values.size(); i++) {
+        if (values[i - 1] < values[i]) {
+            increases++;
+        }
+    }
+
+	printf("%d\n", increases);
+}
